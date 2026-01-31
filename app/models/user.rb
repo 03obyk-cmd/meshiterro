28a61c6d48a7dfda40936308702527413944c274
03obyk-cmd <252697084+03obyk-cmd@users.noreply.github.com>
2026-01-31 11:47:40 +0000

    [Finish]4-9

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
-  has_secure_password
-  has_many :sessions, dependent: :destroy
-
-  normalizes :email_address, with: ->(e) { e.strip.downcase }
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+  :recoverable, :rememberable, :validatable
+  
+  has_many :post_images, dependent: :destroy
+  
 end
